97f7b9b39536e68f5ba5afbaf0eee61b0a3cb12a
Nidhi Singh <nidhi0938.be21@chitkara.edu.in>
2024-08-07 10:00:44 +0530

    first commit

---
+++ utils/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const sessionIdToUserMap=new Map();
+const key="Ali7haider12312331"; 
+// function setUser(id,user){
+//     sessionIdToUserMap.set(id,user);
+// }
+function setUser(user){
+    return jwt.sign({
+        _id:user._id,
+        email:user.Email,
+        username:user.UserName,
+        role:user.Role
+
+    },
+    key
+    );
+}
+// function getUser(id){
+//     return sessionIdToUserMap.get(id);
+// }
+function getUser(token){
+    if (!token) return null;
+    return jwt.verify(token,key);
+}
+
+module.exports={
+    setUser,getUser,
+}
